e6b539c2585864704d5f0c7407862da3f17d7bc7
Kendric Hood <khood5@kent.edu>
2023-05-22 12:08:36 -0400

    fix typo in destination folder name

--- Prepocessing/batchPreprocessing.sh
+++ Prepocessing/batchPreprocessing.sh
@@ -3,63 +3,63 @@
 
 # 10 min
 LENGTH=600
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
 
 # 15 min
 LENGTH=900
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
 
 # 20 min
 LENGTH=1200
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
 
 # 25 min
 LENGTH=1500
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
 
 # 30 min
 LENGTH=1800
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
 
 # 35 min
 LENGTH=2100
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
 
 # 40 min
 LENGTH=2400
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
