66ce5720955b05c3048b982b4dbd17da25793807
Kendric Hood <khood5@kent.edu>
2023-05-22 11:40:49 -0400

    updated scripts to automoate building diffrent time length

--- Prepocessing/batchPreprocessing.sh
+++ Prepocessing/batchPreprocessing.sh
@@ -1,14 +1,65 @@
 #!/bin/bash
 # make sure you are in the right envi (should be conda activate {project name})
 
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_out
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_out
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_out
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_out
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_out
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_out
-# ./runPreprocess.sh data/1.5mm_35.14pN sorted/1.5mm_35.14pN_out
-# ./runPreprocess.sh data/2mm_22.6pN sorted/2mm_22.6pN_out
-# ./runPreprocess.sh data/3mm_9.24pN sorted/3mm_9.24pN_out
-# ./runPreprocess.sh data/5mm_1.46pN sorted/5mm_1.46pN_out
-# ./runPreprocess.sh data/9mm_0.002pN sorted/9mm_0.002pN_out
+# 10 min
+LENGTH=600
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+
+# 15 min
+LENGTH=900
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+
+# 20 min
+LENGTH=1200
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+
+# 25 min
+LENGTH=1500
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+
+# 30 min
+LENGTH=1800
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+
+# 35 min
+LENGTH=2100
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
+
+# 40 min
+LENGTH=2400
+./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_600 $LENGTH
+./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_600 $LENGTH
--- Prepocessing/runPreprocess.sh
+++ Prepocessing/runPreprocess.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # make sure you are in the right envi (should be conda activate {project name})
 
+# example params 
+# INPUT_DIR=""./large_dataset"
+# OUTPUT_DIR="./data/seperatedData"
+# LENGTH=600 a.k.a 10min
 INPUT_DIR=$1 # "./large_dataset"
 OUTPUT_DIR="$2"
-# OUTPUT_DIR="./data/seperatedData"
-
+LENGTH=$3
 
 mkdir -p $OUTPUT_DIR
 # rm -rf $OUTPUT_DIR/*
@@ -14,7 +17,7 @@ i=0
 find "$INPUT_DIR"/ -type f -name "*.xls" -print0 | while read -d $'\0' RAW_DATA; do
     (
         echo "Processing $RAW_DATA"
-        python preprocess.py "$RAW_DATA" "$OUTPUT_DIR" --length 600 --binary -n -0
+        python preprocess.py "$RAW_DATA" "$OUTPUT_DIR" --length $LENGTH --binary -n -0
     )&
     if ((i % $(($CPU_COUNT - 1)) == 0)) ;
     then
@@ -24,18 +27,3 @@ find "$INPUT_DIR"/ -type f -name "*.xls" -print0 | while read -d $'\0' RAW_DATA;
 done 
 wait
 
-# old data
-# python preprocess.py "$INPUT_DIR/9mm_0.002pN" "$OUTPUT_DIR" --length 600 --binary -n
-# python preprocess.py "$INPUT_DIR/5mm_1.46pN" "$OUTPUT_DIR" --length 600 --binary -n
-# python preprocess.py "$INPUT_DIR/3mm_9.24pN" "$OUTPUT_DIR" --length 600 --binary -n
-# python preprocess.py "$INPUT_DIR/2mm_22.6pN" "$OUTPUT_DIR" --length 600 --binary -n
-# python preprocess.py "$INPUT_DIR/1.5mm_35.14pN" "$OUTPUT_DIR" --length 600 --binary -n
-
-# mkdir -p $OUTPUT_DIR/high
-# mkdir -p $OUTPUT_DIR/medium
-# mkdir -p $OUTPUT_DIR/low
-# python preprocess.py "$INPUT_DIR/9mm_0.002pN" "$OUTPUT_DIR/high" --length 600 --binary -n
-# python preprocess.py "$INPUT_DIR/5mm_1.46pN" "$OUTPUT_DIR/high" --length 600 --binary -n
-# python preprocess.py "$INPUT_DIR/3mm_9.24pN" "$OUTPUT_DIR/medium" --length 600 --binary -n
-# python preprocess.py "$INPUT_DIR/2mm_22.6pN" "$OUTPUT_DIR/low" --length 600 --binary -n
-# python preprocess.py "$INPUT_DIR/1.5mm_35.14pN" "$OUTPUT_DIR/low" --length 600 --binary -n
