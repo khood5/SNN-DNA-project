474301f694e767a9bf47627a4ec8369c5e59fa3e
Kendric Hood <khood5@kent.edu>
2023-06-06 18:27:53 -0400

    did some clean up of the repo

--- Prepocessing/batchPreprocessing.sh
+++ Prepocessing/batchPreprocessing.sh
@@ -1,62 +1,7 @@
 #!/bin/bash
 # make sure you are in the right envi (should be conda activate {project name})
 
-# 10 min
-LENGTH=600
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
-
-# 15 min
-LENGTH=900
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
-
-# 20 min
-LENGTH=1200
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
-
-# 25 min
-LENGTH=1500
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
-
-# 30 min
-LENGTH=1800
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
-
-# 35 min
-LENGTH=2100
-./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/800_nM_AR sorted/800_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1200_nM_AR sorted/1200_nM_AR_$LENGTH $LENGTH
-./runPreprocess.sh large_dataset/1800_nM_AR sorted/1800_nM_AR_$LENGTH $LENGTH
-
-# 40 min
-LENGTH=2400
+LENGTH=5000 # full length 
 ./runPreprocess.sh large_dataset/50_nM_AR sorted/50_nM_AR_$LENGTH $LENGTH
 ./runPreprocess.sh large_dataset/100_nM_AR sorted/100_nM_AR_$LENGTH $LENGTH
 ./runPreprocess.sh large_dataset/400_nM_AR sorted/400_nM_AR_$LENGTH $LENGTH
--- dnaDataloader.py
+++ dnaDataloader.py
@@ -9,23 +9,24 @@ class expermentDataloader(Dataset):
         self,
         index_file: str, 
         data_path: str,
-        startIndex = 0, # default to start from begining of list
-        endIndex = None # default to grab the whole list (a.k.k arr[0:None] grabs whole thing)
+        length: None
     ):
         self.root_dir = data_path
-        self.expermentSikeTrainsIndex = np.array(pd.read_csv(index_file,header=None))[startIndex:endIndex]
-        self.spikeTrains = [
-            f"{os.path.join(self.expermentSikeTrainsIndex[i][0])}" for i in range(len(self.expermentSikeTrainsIndex)) 
+        self.expermentMovieFrames = np.array(pd.read_csv(index_file,header=None))
+        self.frames = [
+            f"{os.path.join(self.expermentMovieFrames[i][0])}" for i in range(len(self.expermentMovieFrames)) 
         ]
-        self.targets = self.expermentSikeTrainsIndex[:, 1]
+        self.targets = self.expermentMovieFrames[:, 1]
+        self.length = length
 
     def __getitem__(self, index):
-        inputCSVlines = pd.read_csv(os.path.join(self.root_dir,self.spikeTrains[index]), header=None).to_numpy()
-        targetCSVLines = self.targets[index]
-        return inputCSVlines.flatten(), np.array([targetCSVLines])
+        expermentMovieFrames = pd.read_csv(os.path.join(self.root_dir,self.frames[index]), header=None).to_numpy()
+        expermentMovieFrames = expermentMovieFrames[:self.length]
+        totalNumberOfEvents = self.targets[index]
+        return expermentMovieFrames.flatten(), np.array([totalNumberOfEvents])
 
     def __len__(self):
-        return len(self.expermentSikeTrainsIndex)
+        return len(self.expermentMovieFrames)
 
 # takes a expermentDataset and a rhs size splits the dataset into 2 set with rhs matching the desierd size and lhs having the remmaing elements 
 def addData(lhs: list, rhs: list, expermentDataset: expermentDataloader, rhsSize=None):
